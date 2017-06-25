5c9387581506b8710c66973275c4cebfbd615674
Vinesh Kannan <vingkan@gmail.com>
2017-06-25 02:40:43 +0000

    Read in coordinates

--- Location.java
+++ Location.java
@@ -1,21 +1,52 @@
 import java.util.*;
 
-abstract public class Location {
+public class Location {
     
+    private double lat;
+    private double lng;
     private String name = "Unnamed Location";
-
+    
     public Location(){
         
     }
-
+    
     public Location(String name){
         this.name = name;
+    }
+
+    public Location(double lat, double lng){
+        this.lat = lat;
+        this.lng = lng;
+    }
+    
+    public Location(double lat, double lng, String name){
+        this.lat = lat;
+        this.lng = lng;
+        this.name = name;
+    }
+    
+    public Location(String name, int x, int y){
+        this.name = name;
+    }
+    
+    public void doInteractions(List<Person> people){
+        
+    }
+    
+    public static double getDistance(Location l1, Location l2){
+        return 0.0;   
+    }
+    
+    public double getLat(){
+        return this.lat;
+    }
+    
+    public double getLng(){
+        return this.lng;
     }
     
     public String getName(){
         return this.name;
     }
-    
-    abstract void doInteractions(List<Person> people);
 
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.io.*;
+import java.util.regex.*;
 
 /*
  * F.I.S.H.
@@ -9,12 +10,27 @@ public class Main {
     
     public static int TURNS = 30;
     public static int MAX_TURNS = 500;
-    public static int GRID_SIZE = 20;
+    public static int GRID_SIZE = 5;
     public static int FAMILY_SIZE = 4;
 
     public static void main(String[] args){
         
         City city = new City("Scise City");
+        
+        List<double[]> coords = new ArrayList<double[]>();
+        String input = "41.25871065195757,-96.00540161132812$41.23444575262552,-95.97038269042969$41.209655278807034,-96.02668762207031$";
+        String[] coordStrs = input.split(Pattern.quote("$"));
+        for(String str : coordStrs){
+            String[] pairStr = str.split(Pattern.quote(","));
+            double[] pair = new double[2];
+            pair[0] = Double.parseDouble(pairStr[0]);
+            pair[1] = Double.parseDouble(pairStr[1]);
+            coords.add(pair);
+        }
+        /*for(double[] pair : coords){
+            System.out.println(pair[0] + " <---> " + pair[1]);
+        }*/
+        
         
         for(int x = 0; x < GRID_SIZE; x++){
             for(int y = 0; y < GRID_SIZE; y++){
